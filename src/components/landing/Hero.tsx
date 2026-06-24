7221c6fe5d3a09741434c70054b6f8e6bac1507d
Gowreesh V T <135145326+Gowreesh-VT@users.noreply.github.com>
2026-06-24 13:19:12 +0530

    change the landing page ui

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -34,9 +34,9 @@ export default function Section1() {
   return (
     <>
       <div className="w-full min-h-screen flex justify-center items-center bg-[#FFF8E7]">
-        <div className="w-[92%] max-w-[1200px] bg-white rounded-4xl shadow-[0_8px_30px_rgba(0,0,0,0.03)]  min-h-[680px]      flex flex-col relative">
+        <div className="w-[92%] max-w-300 bg-white rounded-4xl shadow-[0_8px_30px_rgba(0,0,0,0.03)] min-h-170 flex flex-col relative">
           {/* NAVBAR */}
-          <nav className="w-full  h-[102px]   rounded-t-[28px] shadow-[0px_6px_30.1px_rgba(186,185,185,0.25)] relative">
+          <nav className="w-full  h-25.5   rounded-t-[28px] shadow-[0px_6px_30.1px_rgba(186,185,185,0.25)] relative">
             <span className="left-20 top-7 text-[30px] font-bold text-black tracking-widest uppercase absolute">
               FFCS
             </span>
@@ -74,12 +74,10 @@ export default function Section1() {
                   Get Started
                 </button>
                 {open && (
-                  <div className="fixed  inset-0 flex items-center justify-center bg-black/30 z-50">
+                  <div className="fixed inset-0 flex items-center justify-center bg-black/30 z-50">
                     {/* Modal */}
-                    <div className="flex items-center justify-center w-[949px] h-[511px] bg-[#FFFCEE] rounded-[20px] shadow-xl p-8 relative">
-                      {/* Close Button */}
-                      <div className="relative bg-[#FAFAFA] w-[900px] h-[459px] flex flex-col items-center rounded-[20px] p-10 shadow-[4px_4px_4px_rgba(191,191,191,0.25)]">
-                        {/* Close Button */}
+                    <div className="flex items-center justify-center w-237.25 h-127.75 bg-[#FFFCEE] rounded-[20px] shadow-xl p-8 relative">
+                      <div className="relative bg-[#FAFAFA] w-225 h-114.75 flex flex-col items-center rounded-[20px] p-10 shadow-[4px_4px_4px_rgba(191,191,191,0.25)]">
                         <button
                           onClick={() => setOpen(false)}
                           className="absolute top-6 right-6 text-gray-500 hover:text-black text-[28px]"
@@ -87,24 +85,20 @@ export default function Section1() {
                           ✕
                         </button>
 
-                        {/* Title */}
-                        <h2 className="text-[32px] font-semibold text-center mb-2 absolute top-[50px]">
+                        <h2 className="text-[32px] font-semibold text-center pb-2 top-12.5">
                           Welcome back, {session?.user?.name}!
                         </h2>
 
-                        {/* Divider */}
-                        <div className="w-[700px] h-[1px] bg-gray-300 mb-4 absolute top-[90px]"></div>
+                        <div className="w-175 h-px bg-gray-300 mb-4 absolute top-22.5"></div>
 
-                        {/* Subtitle */}
-                        <p className=" text-center text-[20px] mb-12 absolute top-[110px]">
+                        <p className=" text-center text-[20px] mb-12 absolute top-27.5">
                           Choose what you&apos;d like to do next
                         </p>
 
-                        {/* Options */}
-                        <div className="flex gap-14 absolute top-[180px]">
-                          {/* Create new */}
+                        <div className="flex gap-14 absolute top-45">
+
                           <button
-                            className="flex flex-col items-center justify-center bg-[#E9F3E8] border-[5px] border-[#D4F4E6] rounded-[16px] p-6 w-[290px] h-[200px] shadow hover:bg-green-200 transition"
+                            className="flex flex-col items-center justify-center bg-[#E9F3E8] border-[5px] border-[#D4F4E6] rounded-2xl p-6 w-72.5 h-50 shadow hover:bg-green-200 transition"
                             onClick={() => { setOpen(false); router.push('/preferences'); }}
                           >
                             <Image
@@ -119,7 +113,6 @@ export default function Section1() {
                             </p>
                           </button>
 
-                          {/* View saved */}
                           <button
                             className="flex flex-col items-center justify-center bg-[#E9D5FF] border-[#F2D8FE] border-[5px] rounded-[16px] p-6 w-[290px] h-[200px] shadow hover:bg-purple-300 transition"
                             onClick={() => { setOpen(false); router.push("/saved"); }}
@@ -140,7 +133,7 @@ export default function Section1() {
                     </div>
                   </div>
                 )}
-                <button className="rounded-[8px] w-[180px] h-[50px]  border-[1.5px] border-[#A0C4FF] px-8 py-3.5 text-[14px] font-bold text-black bg-white hover:bg-blue-50 transition-colors shadow-sm">
+                <button className="rounded-lg w-45 h-12.5  border-[1.5px] border-[#A0C4FF] px-8 py-3.5 text-[14px] font-bold text-black bg-white hover:bg-blue-50 transition-colors shadow-sm">
                   Slot View
                 </button>
               </div>
@@ -154,7 +147,7 @@ export default function Section1() {
         </div>
 
         {/* ===== SPACER ===== */}
-        <div className="h-[80px]" />
+        <div className="h-20" />
       </div>
     </>
   );
